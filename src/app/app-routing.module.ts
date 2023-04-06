baa836dc527078340dfdb29fba3ef750bbff2bba
yangj35 <yangjal@umich.edu>
2023-04-06 00:21:18 -0400

    started on arthritis page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './home/home/about.component';
+import { ArthritisComponent } from './arthritis/arthritis.component';
 import { HomeComponent } from './home/home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'arthritis', component: ArthritisComponent },
 ];;
 
 @NgModule({
---
+++ src/app/arthritis/arthritis.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'arthritis',
+  templateUrl: './arthritis.component.html',
+  styleUrls: ['./arthritis.component.css']
+})
+export class ArthritisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
